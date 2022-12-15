faae820607388ef0e46872867513faa77d1607a3
Ryan Block <rblock@gmail.com>
2022-12-15 11:23:11 -0800

    Add configuration via env vars, pass options to telemetry endpoint

--- src/begin-telemetry/lib.js
+++ src/begin-telemetry/lib.js
@@ -1,4 +1,32 @@
-let debug = process.env.BEGIN_DEBUG
+let debug
 let log = (...logs) => debug ? console.log('[Begin telemetry extension]', ...logs) : null
+let knownConfig = [ 'debug', 'url', 'telemetryTypes', 'telemetryTimeoutMs', 'telemetryListenerPort' ]
 
-module.exports = { log }
+function getConfig () {
+  try {
+    let { TELEMETRY_CONFIG = '{}', TELEMETRY_DEBUG, TELEMETRY_URL } = process.env
+
+    let config = JSON.parse(TELEMETRY_CONFIG)
+    if (config.debug || TELEMETRY_DEBUG) debug = true
+    if (TELEMETRY_URL) config.url = TELEMETRY_URL
+    if (!config.url) throw ReferenceError('Telemetry endpoint not found')
+
+    // Set config defaults
+    config.rate = config.rate || 333 // Flush the event queue; default up to 3x/second
+    config.telemetryTypes = config.telemetryTypes || [ 'platform', 'function' ]
+    config.telemetryTimeoutMs = config.telemetryTimeoutMs || 25
+    config.telemetryListenerPort = config.telemetryListenerPort || 4243
+
+    // Arbitrary options to pass to the telemetry API
+    let options = JSON.parse(TELEMETRY_CONFIG)
+    knownConfig.forEach(opt => delete options[opt])
+
+    return { config, options }
+  }
+  catch (err) {
+    console.error('Begin telemetry config error', err)
+    process.exit(1)
+  }
+}
+
+module.exports = { log, getConfig }
--- src/begin-telemetry/listener.js
+++ src/begin-telemetry/listener.js
@@ -1,9 +1,9 @@
 let http = require('http')
 let host = process.env.AWS_LAMBDA_FUNCTION_NAME ? 'sandbox.localdomain' : 'localhost'
-let port = 4243 // FIXME: this will conflict in the Sandbox invocation model
 let eventQueue = []
 
-function eventListener () {
+function eventListener (config) {
+  let { telemetryListenerPort: port } = config
   let server = http.createServer()
   server.on('request', (req, res) => {
     let raw = []
